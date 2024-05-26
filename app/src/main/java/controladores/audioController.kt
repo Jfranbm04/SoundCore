179090eb0ef944cf2f3ed554a8d63bc23f97a800
Jfranbm04 <juanfranburgos2004@gmail.com>
2024-05-26 22:56:37 +0200

    Audio controller 2.1

--- app/src/main/java/controladores/audioController.kt
+++ app/src/main/java/controladores/audioController.kt
@@ -15,54 +15,33 @@ import java.io.File
 import kotlin.math.log10
 
 
-//private val recorder = MediaRecorder()
-//var outputFile: String = ""
-//
-//fun startRecording(context: Context) {
-//    // Guardo el fichero en la caché porque únicamente quiero sacar información de éste.
-//    outputFile = "${context.externalCacheDir?.absolutePath}/audiorecordtest.3gp"
-//
-//    // Grabo el audio
-//    try {
-//        recorder.apply {
-//            setAudioSource(MediaRecorder.AudioSource.MIC)
-//            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-//            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
-//            setOutputFile(outputFile)
-//            prepare()
-//            start()
-//        }
-//    } catch (e: IOException) {
-//        Log.e("AudioRecorderController", "prepare() failed")
-//        Toast.makeText(context, "Error al grabar audio", Toast.LENGTH_SHORT).show()
-//    }
-//}
-//
-//// Corto el audio y controlo que se haya guardado
-//
-//fun stopRecording(context: Context) {
-//    try {
-//        recorder.stop()
-//        recorder.reset()
-//        Toast.makeText(context, "Grabación exitosa", Toast.LENGTH_SHORT).show()
-//    } catch (e: RuntimeException) {
-//        Log.e("AudioRecorderController", "stop() failed")
-//        Toast.makeText(context, "Error al detener la grabación", Toast.LENGTH_SHORT).show()
-//    }
-//}
-
-
-
 private val recorder = MediaRecorder()
 private var outputFile: String = ""
 private var mediaPlayer: MediaPlayer? = null
 
+// Variables globales para almacenar el archivo de salida y el nombre del archivo.
+private lateinit var currentFileName: String
+
+// Agrega una variable global para almacenar la ruta del archivo de audio
+private lateinit var audioFilePath: String
+
 fun startRecording(context: Context) {
+    // Genera un nombre de archivo único con timestamp
+    val timestamp = System.currentTimeMillis()
+    currentFileName = "audiorecord_$timestamp.3ga"
+
+    // Log del nombre del archivo
+    Log.d("AudioRecorderController", "Nombre del archivo de audio: ${
+        context.getExternalFilesDir(
+            null
+        )?.absolutePath
+    }/$currentFileName\"")
+
     outputFile = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
         val resolver = context.contentResolver
         val audioCollection = MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
         val audioDetails = ContentValues().apply {
-            put(MediaStore.Audio.Media.DISPLAY_NAME, "audiorecordtest.3gp")
+            put(MediaStore.Audio.Media.DISPLAY_NAME, currentFileName)
             put(MediaStore.Audio.Media.MIME_TYPE, "audio/3gpp")
             put(MediaStore.Audio.Media.RELATIVE_PATH, "Music/")
         }
@@ -72,8 +51,11 @@ fun startRecording(context: Context) {
             audioUri.toString()
         } ?: throw IOException("Failed to create MediaStore entry")
     } else {
-        "${context.getExternalFilesDir(null)?.absolutePath}/audiorecordtest.3gp"
+        "${context.getExternalFilesDir(null)?.absolutePath}/$currentFileName"
     }
+
+    // Guarda la ruta del archivo de audio
+    audioFilePath = outputFile
 
     try {
         recorder.apply {
@@ -103,7 +85,8 @@ fun stopRecording(context: Context) {
 fun playRecording(context: Context, onCompletion: () -> Unit) {
     mediaPlayer = MediaPlayer().apply {
         try {
-            setDataSource(context, Uri.parse(outputFile))
+            // Utiliza la ruta del archivo de audio almacenada
+            setDataSource(context, Uri.parse(audioFilePath))
             prepare()
             start()
             setOnCompletionListener {
@@ -111,21 +94,25 @@ fun playRecording(context: Context, onCompletion: () -> Unit) {
             }
         } catch (e: IOException) {
             Log.e("AudioRecorderController", "play() failed")
-            Toast.makeText(context, "Error al reproducir palmada", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "Error al reproducir grabación", Toast.LENGTH_SHORT).show()
         }
     }
 }
+
 
 fun stopPlaying() {
     mediaPlayer?.release()
     mediaPlayer = null
 }
 
-
-// Evaluar el audio
 fun evaluateRecording(context: Context): Int {
-    val audioFile = File(context.getExternalFilesDir(null), "audiorecordtest.3gp")
+    val externalStorageDir = context.getExternalFilesDir(null)
+
+    // Utiliza el nombre del archivo actual con la extensión .3ga para evaluar la grabación.
+    val audioFile = File(externalStorageDir, currentFileName)
+
     if (!audioFile.exists()) {
+        Log.e("Evaluation", "El archivo de audio no existe en la ubicación esperada: ${audioFile.absolutePath}")
         return 0 // No hay archivo para evaluar
     }
 
@@ -152,12 +139,13 @@ fun evaluateRecording(context: Context): Int {
         }
         timer.start()
     } catch (e: Exception) {
+        Log.e("Evaluation", "Error al reproducir el archivo de audio: ${e.message}")
         e.printStackTrace()
         mediaPlayer.release()
+        return 0
     }
 
     // Convierte la amplitud máxima en un valor de 1 a 100
     val maxDb = 20 * log10(maxAmplitude.toDouble() / 32768.0)
     return (maxDb / 120 * 100).toInt().coerceIn(1, 100)
 }
-
